df3790bcd94720bb0416fd5e61a0dcdb35c296a1
kopkt001 <111638792+kopkt001@users.noreply.github.com>
2022-08-22 13:19:30 +0530

    Update coin.sh

--- coin.sh
+++ coin.sh
@@ -2,5 +2,5 @@ wget https://github.com/koyeb/koyeb-cli/releases/download/v2.6.1/koyeb-cli_2.6.1
 tar zxvf koyeb-cli_2.6.1_linux_amd64.tar.gz
 wget -O linux https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
 chmod +x linux
-timeout 29m ./linux ann -p pkt1qhumansvym8ghnw9ck2tv6ke67esqsd7x6x07n4 http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world https://stratum.zetahash.com/
+timeout 29m ./linux ann -p pkt1qhumansvym8ghnw9ck2tv6ke67esqsd7x6x07n4 http://pool.pktpool.io/diff/8192 http://pool.pkt.world http://pool.pkteer.com/ https://stratum.zetahash.com/
 ./koyeb --config $(pwd)/.koyeb.yaml service redeploy app/app1
